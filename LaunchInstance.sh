1fae20b4c046e85e837a8061f7efb58ec80c3c73
rushikeshsawant143 <65754477+rushikeshsawant143@users.noreply.github.com>
2021-06-16 13:00:16 +0530

    Update LaunchInstance.sh

--- LaunchInstance.sh
+++ LaunchInstance.sh
@@ -4,4 +4,4 @@ ENVIRONMENT_NAME="$TEST_ENV"
 ls
 
 
-aws cloudformation deploy --stack-name ec2instance --template-file 'EC2_instance.yml' --parameter-overrides EnvironmentName=$ENVIRONMENT_NAME Keyname=awskey2 --capabilities CAPABILITY_IAM --region $AWS_REGION
+aws cloudformation deploy --stack-name ec2instance --template-file 'EC2_instance.yml' --parameter-overrides EnvironmentName=$ENVIRONMENT_NAME Keyname=awskey2 --capabilities CAPABILITY_IAM --region $AWS_DEFAULT_REGION
